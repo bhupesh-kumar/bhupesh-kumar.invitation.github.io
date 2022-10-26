b22b94706cc726afd14269dbdb7ec4ffd277adb6
bhupesh-kumar <sahu95bhupesh@gmail.com>
2022-10-26 22:46:53 +0530

    invitation ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'invitation';
+export class AppComponent implements OnInit{
+  public showpop: boolean = false;
+  public envelopEnable: boolean = true;
+  ngOnInit(): void {
+    setInterval(() => {
+      if (window.innerHeight > window.innerWidth) {
+        this.showpop = true;
+      } else {
+        this.showpop = false;
+      }
+    }, 500);
+  }
+
+  clickEnvelop() {
+    this.envelopEnable = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
