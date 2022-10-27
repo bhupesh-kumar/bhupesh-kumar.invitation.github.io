8fefd56d496f22aa22721a0af446931005885bc4
bhupesh-kumar <sahu95bhupesh@gmail.com>
2022-10-27 23:38:33 +0530

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,19 @@ export class AppComponent implements OnInit{
 
   clickEnvelop() {
     this.envelopEnable = false;
+    const elem:any = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
   }
 
 }
